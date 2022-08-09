50c8939de48be00caf5bcea493093e182ffb2f15
4teeen <artmvashunin14@gmail.com>
2022-08-09 12:49:58 +0300

    Добавьте файлы проекта.

---
+++ Prak 7/Listik.cs
@@ -0,0 +1,189 @@
+﻿using System.Collections;
+
+namespace Prak_7
+{
+    public class Listik<T> : IEnumerable<T>
+    {
+        private T[] _items;
+        private int _length, _capacity;
+        const int DefCapacity = 4;
+        public int Length
+        {
+            get { return _length; }
+        }
+
+        public int Capacity
+        {
+            get { return _capacity; }
+            set
+            {
+                if (value < _length)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                _capacity = value;
+            }
+        }
+        public Listik()
+        {
+            _items = new T[0];
+        }
+
+        public Listik(int capacity)
+        {
+            Capacity = capacity;
+            _items = new T[Capacity];
+        }
+
+        public Listik(System.Collections.Generic.IEnumerable<T> collection)
+        {
+            _items = collection.ToArray();
+        }
+
+        private void IncreaseCapacity()
+        {
+            if (_capacity == 0)
+            {
+                _items = new T[2];
+                _capacity = _items.Length;
+            }
+            else 
+            {
+                Array.Resize(ref _items, _capacity * 2);
+                _capacity = _items.Length;
+            }
+        }
+
+        public void Add(T item)
+        {
+            if (_length == _capacity)
+            {
+                IncreaseCapacity();
+            }
+
+            if (_length == 0)
+            {
+                _items[0] = item;
+                _length++;
+            }
+
+            else
+            {
+                if (_length < _capacity)
+                {
+                    _items[_length] = item;
+                    _length++;
+                }
+            }
+        }
+
+        public void AddRange(IEnumerable<T> collection)
+        {
+            T[] temparr = collection.ToArray();
+            if (_length == 0)
+            {
+                IncreaseCapacity();
+                _items = temparr;
+                _length = _items.Length;
+            }
+            else
+            {
+                for (int i = 0; i < temparr.Length; i++)
+                {
+                    if (_length < _capacity)
+                    {
+                        _items[_length] = temparr[i];
+                        _length++;
+                    }
+                    else
+                    {
+                        IncreaseCapacity();
+                        _items[_length] = temparr[i];
+                        _length++;
+                    }
+                }
+            }
+        }
+
+        public void Clear()
+        {
+            _length = 0;
+            _items = new T[0];
+        }
+
+        public int IndexOf(T element)
+        {
+            for (int i = 0; i < _length; i++)
+            {
+                if (object.Equals(_items[i], element))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public void Remove(int index)
+        {
+            if (index < 0 || index >= _length)
+                throw new IndexOutOfRangeException();
+            _items[index] = default;
+            for (int i = index; i < _length - 1; i++)
+            {
+                _items[i] = _items[i+1];
+            }
+            _items[_items.Length-1] = default;
+            _length--;
+            Array.Resize(ref _items, _length);
+        }
+
+        public void RemoveAll(T element)
+        {
+            for (int i = 0; i < _length; i++)
+            {
+                if (object.Equals(_items[i], element))
+                {
+                    Remove(i);
+                    i--;
+                }
+            }
+        }
+
+        public void Reverse()
+        {
+            int x;
+            if (_length % 2 == 0)
+            {
+                x = _length / 2;
+                for (int i = 0; i < x; i++)
+                {
+                    T temp = _items[i];
+                    _items[i] = _items[_length - 1 - i];
+                    _items[_length - 1 - i] = temp;
+                }
+            }
+            else if (_length % 2 != 0)
+            {
+                x = _length / 2 + 1;
+                for (int i = 0; i < x; i++)
+                {
+                    T temp = _items[i];
+                    _items[i] = _items[_length - 1 - i];
+                    _items[_length - 1 - i] = temp;
+                }
+            }
+        }
+
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < _length; i++)
+                yield return _items[i];
+        }
+    }
+}
---
+++ Prak 7/Program.cs
@@ -0,0 +1,111 @@
+﻿using Prak_7;
+
+
+Listik<int> list1 = new Listik<int>();
+Listik<int> list2 = new Listik<int>();
+int[] arr2 = new int[] { 1, 2, 3, 4 };
+list2.AddRange(arr2);
+
+
+Console.Write("Before Add: ");
+foreach (var item in list1)
+{
+    Console.Write(item + " ");
+}
+Console.WriteLine(Environment.NewLine + $"Capacity = {list1.Capacity}");
+
+
+list1.Add(3);
+list1.Add(9);
+Console.Write(Environment.NewLine + "After Add: ");
+foreach (var item in list1)
+{
+    Console.Write(item + " ");
+}
+Console.WriteLine(Environment.NewLine + $"Capacity = {list1.Capacity}");
+
+
+int[] arr1 = new int[] { 7, 7, 4, 2, 8 };
+list1.AddRange(arr1);
+Console.Write(Environment.NewLine + "After AddRange(array): ");
+foreach (var item in list1)
+{
+    Console.Write(item + " ");
+}
+Console.WriteLine(Environment.NewLine + $"Capacity = {list1.Capacity}");
+
+
+list1.AddRange(list2);
+Console.Write(Environment.NewLine + "After AddRange(list2): ");
+foreach (var item in list1)
+{
+    Console.Write(item + " ");
+}
+Console.WriteLine(Environment.NewLine + $"Capacity = {list1.Capacity}");
+
+
+Console.WriteLine(Environment.NewLine + $"Index of 8 is {list1.IndexOf(8)}");
+Console.WriteLine($"Index of 79 is {list1.IndexOf(0)}");
+Console.WriteLine();
+
+
+list1.Remove(1);
+Console.Write("After Remove first element: ");
+foreach (var item in list1)
+{
+    Console.Write(item + " ");
+}
+Console.WriteLine(Environment.NewLine + $"Capacity = {list1.Capacity}");
+
+
+list1.RemoveAll(7);
+Console.Write(Environment.NewLine + "After RemoveAll(7): ");
+foreach (var item in list1)
+{
+    Console.Write(item + " ");
+}
+Console.WriteLine(Environment.NewLine + $"Capacity = {list1.Capacity}");
+
+
+list1.Reverse();
+Console.Write(Environment.NewLine + "After Reverse: ");
+foreach (var item in list1)
+{
+    Console.Write(item + " ");
+}
+Console.WriteLine(Environment.NewLine + $"Capacity = {list1.Capacity}");
+
+
+list1.Clear();
+Console.Write(Environment.NewLine + "After Clear: ");
+foreach (var item in list1)
+{
+    Console.Write(item + " ");
+}
+Console.WriteLine(Environment.NewLine + $"Capacity = {list1.Capacity}");
+
+//
+
+//var stream = File.OpenRead(@"C:\Users\R3-D3\source\repos\Prak 7\Prak 7\Listik.cs");
+//stream.Dispose();
+//Console.WriteLine(stream.CanRead);
+
+//Foo();
+
+//void Foo()
+//{
+//    var test = new Test();
+//    test.Foo();
+//    Console.WriteLine(test._list.Count);
+//}
+
+//class Test
+//{
+//    public List<int> _list;
+
+//    public void Foo()
+//    {
+//        _list = new List<int>();
+//        var list = _list;
+//    }
+//}
